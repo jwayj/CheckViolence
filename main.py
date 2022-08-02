b5f55841d4bf452e048e57991a859ece8db7772a
ezez-refer <jjn00712@gmail.com>
2022-08-02 09:39:34 +0900

    feat: KRBERT 모델 추가에 따른 학습 실행 코드 수정 및 flask 추가

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from utils import check_gpu
+from flask import Flask, render_template, request
+
 from dataloader import Dataset
 from model import Models
+from transformers import BertTokenizer
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/isViolence')
+def Predict():
+    input_text = request.args.get('input_text')
+    
+    print("input text : ", input_text)
+    
+    if input_text == None:
+        return render_template('isViolence.html', Output = '')
+
+    ModelOutput = model.inference(input_text)
+    
+    print("output : ", ModelOutput)
+
+    return render_template('isViolence.html', Output = ModelOutput)
 
 if __name__ == '__main__':
 
     # 모델 생성, 학습, 모델 저장
-    model = Models(num_labels = 2)
+    model = Models('krbert', num_labels = 2)
     dataset = Dataset()
     dataset.set_dataset('train')    # train dataset load
     dataset.set_dataset('test')     # test dataset load
+    dataset.set_tokenizer(BertTokenizer.from_pretrained("snunlp/KR-BERT-char16424"))
+    print(dataset.get_tokenizer())
     train, valid, test = dataset.get_dataloader()   # train/valid/test dataloader 생성
     model.BERT()    # 모델 생성
+    model.about_model()
     model.train(train, valid, epochs = 1)    # 학습
     model.test(test)    # 테스트
     model.save_model()
@@ -18,4 +41,6 @@ if __name__ == '__main__':
     # # 모델 생성, 모델 불러오기, 문장 폭력성 여부 추론
     # model = Models(num_labels = 2)
     # model.load_model()
-    # model.inference('이런건 걍 돈없고 멍청한 전라도 여자들 겨냥한 상술이지')
+    # app.run(host='0.0.0.0', port=5000)
+    # # model.inference('이런건 걍 돈없고 멍청한 전라도 여자들 겨냥한 상술이지')
+
