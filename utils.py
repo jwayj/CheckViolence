02e8a85e194e5ac4313940967475dda90b5ee05a
ezez-refer <jjn00712@gmail.com>
2022-08-03 17:28:54 +0900

    feat: split_sentence 메소드 추가
    - 카카오톡 채팅방의 채팅 파일을 입력으로 받았을 때 모델의 입력으로 적절하게 문장 단위로 구분하여 리스트로 반환
    
    ex) ['name', 'time', 'sentence'], ['홍길동', '오전 00:00', '안녕하세요']

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import datetime
+import re
 
 
 def check_gpu():
@@ -36,3 +37,58 @@ def format_time(elapsed):
     
     # Format as hh:mm:ss
     return str(datetime.timedelta(seconds=elapsed_rounded))
+
+
+def split_sentence(chattings):
+    idx = 0
+    result = []
+    flag = False
+    while idx < len(chattings):
+        # '--------------- 2020년 9월 13일 일요일 ---------------' 같은 시간 문자열은 continue
+        if re.findall(r'[-]+ [\d]+년 [\d]+월 [\d]+일 [가-힣]요일 [-]+', chattings[idx]):
+            # print("continue : ", chattings[idx])
+            idx += 1
+            continue
+        
+        if not chattings[idx]:
+            idx += 1
+            continue
+
+        if flag: # 채팅 내용 시작 후
+            if chattings[idx][0] == '[': # [이름] [시간] 형식으로 되어 있는 문장인 경우
+                name_time = re.findall(r'\[[^\]]*\] \[[^\]]*\]', chattings[idx])
+
+                try:
+                    name, time = re.findall(r'\[[^\]]*\]', name_time[0])
+                except:
+                    # print("except됨")
+                    # print("index : ", idx)
+                    # print("문장 : ", chattings[idx])
+                    result[-1][2] += ' ' + chattings[idx].replace('\n', '')
+
+                sentence = re.split(r'\[[^\]]*\] \[[^\]]*\]', chattings[idx])[-1][1:].replace('\n', '')
+                result.append([name[1:-1], time[1:-1], sentence])
+
+            else: # 이전 채팅에 붙어야 하는 문장인 경우
+                result[-1][2] += ' ' + chattings[idx].replace('\n', '')
+                
+        
+        else: # 채팅 내용 시작 전
+            if chattings[idx][0] == '[':
+                flag = True
+                name_time = re.findall(r'\[[^\]]*\] \[[^\]]*\]', chattings[idx])
+
+                try:
+                    name, time = re.findall(r'\[[^\]]*\]', name_time[0])
+                except:
+                    # print("except됨")
+                    # print("index : ", idx)
+                    # print("문장 : ", chattings[idx])
+                    result[-1][2] += ' ' + chattings[idx].replace('\n', '')
+
+                sentence = re.split(r'\[[^\]]*\] \[[^\]]*\]', chattings[idx])[-1][1:].replace('\n', '')
+                result.append([name[1:-1], time[1:-1], sentence])
+            
+        idx += 1
+
+    return result
